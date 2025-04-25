d7c946e1b03bf964092c898d3d72dae41b153109
Let <leticiacarvalhosilva2006@gmail.com>
2025-04-25 13:58:52 -0300

    Aula16-lab

---
+++ Aula16-Lab/ArvoreBinaria.java
@@ -0,0 +1,73 @@
+public class ArvoreBinaria {
+    private No raiz;
+    //construtor padrão
+    public boolean arvoreVazia(){
+        return raiz == null;
+    }
+    public void insere(int info){
+        No novo = new No(info);
+        if(arvoreVazia()){
+            raiz = novo;
+        }
+        else{
+            insereRec(novo, raiz);
+        }
+    }
+    public void insereRec(No novo, No atual){
+        if (novo.getInfo() > atual.getInfo()){
+            if(atual.getDireita() == null)
+                atul.setDireita(novo);
+            else
+                insereRec(novo, atual.getDireita());
+        }
+        else{
+            if(atual.getEsquerda() == null)
+                atul.setEsquerda(novo);
+            else
+                insereRec(novo, atual.getEsquerda());
+        }
+    }
+    //usar o percurso em ordem simetrica para construir o toString
+    @Override
+    public String toString(){
+        if(arvoreVazia()) return "arvore vazia";
+        return toString(raiz);
+    }
+    private String toStringRec(No atual){
+        String s = "";
+        if (atual.getEsquerda() != null)
+            s = s + toStringRec(atual.getEsquerda());
+        s = s + atrual + " ";
+        if (atual.getDireita() != null)
+            s =  s + toStringRec(atual.getDireita());
+        return s;
+    }
+}
+class No{
+    private int info;
+    private No esquerda;
+    private No direita;
+    public No(int info) {
+        this.info = info;
+    }
+    public No getEsquerda() {
+        return esquerda;
+    }
+    public void setEsquerda(No esquerda) {
+        this.esquerda = esquerda;
+    }
+    public No getDireita() {
+        return direita;
+    }
+    public void setDireita(No direita) {
+        this.direita = direita;
+    }
+    public int getInfo() {
+        return info;
+    }
+    @Override
+    public String toString(){
+        //truque para promover info a uma string
+        return info + "";
+    }
+}
