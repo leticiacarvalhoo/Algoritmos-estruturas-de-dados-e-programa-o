659a6a183ee8eaa334ab98f74f873758b4c09f37
Let <leticiacarvalhosilva2006@gmail.com>
2025-05-16 22:44:21 -0300

    Aula19 - Lab

---
+++ Aula20-Lab/Entrada.java
@@ -0,0 +1,10 @@
+public class Entrada<K, V> {
+    K key;
+    V value;
+    Entrada<K, V> proximo;
+    public Entrada(K key,V value){
+        this.key = key;
+        this.value = value;
+        this.proximo = null;
+    }
+}
---
+++ Aula20-Lab/TesteHash.java
@@ -0,0 +1,14 @@
+public class TesteHash {
+    public static void main(String[] args) {
+        NossoHash<String, String> mapa = new NossoHash();
+        mapa.put ("12345", "aaaa");
+        mapa.put ("6789", "lelets");
+        mapa.put ("4357", "lysandra");
+        mapa.put ("9876", "mateusete");
+        mapa.put ("555555", "oi");
+        mapa.put ("7777", "hi");
+        mapa.put ("9999", "hello");
+
+        mapa.imprime();
+    }
+}
